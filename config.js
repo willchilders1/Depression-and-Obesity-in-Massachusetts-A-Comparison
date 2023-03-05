26609a862cf6c8a9b866f9b22707031fde604513
Will Childers <WSCH224@uky.edu>
2023-03-05 15:13:16 -0500

    location & new geojson data
    added location data and geojson with only relevant towns

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {
-    style: 'mapbox://styles/mapbox/streets-v11',
-    accessToken: 'YOUR_ACCESS_TOKEN',
+    style: 'mapbox://styles/willchilders1/cleotdexr00m001qxnol5hhbn',
+    accessToken: 'pk.eyJ1Ijoid2lsbGNoaWxkZXJzMSIsImEiOiJjbGVvdWR3cGkwMmljM3JwNGt6NTY5eGQxIn0.hp7TC4XE9jylFxvvBn4YVA',
     showMarkers: true,
     markerColor: '#3FB1CE',
     //projection: 'equirectangular',
@@ -13,18 +13,18 @@ var config = {
     title: 'The Title Text of this Story',
     subtitle: 'A descriptive and interesting subtitle to draw in the reader',
     byline: 'By a Digital Storyteller',
-    footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
+    footer: 'Sources: <a href="https://commons.wikimedia.org/wiki/File:Newton_City_Hall,_Massachusetts.jpg">Kenneth C. Zirkel</a>, <a href="https://creativecommons.org/licenses/by-sa/4.0">CC BY-SA 4.0</a>, via Wikimedia Commons, <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
     chapters: [
         {
             id: 'slug-style-id',
             alignment: 'left',
             hidden: false,
-            title: 'Display Title',
-            image: './path/to/image/source.png',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            title: 'Depression in Amherst',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/b/b0/Town_Hall%2C_Amherst_MA.jpg',
+            description: 'Population: 19,065. This will talk about the prevalence of depression on college campuses',
             location: {
-                center: [-122.418398, 37.759483],
-                zoom: 8.5,
+                center: [-72.51884558, 42.375687],
+                zoom: 11,
                 pitch: 60,
                 bearing: 0
             },
@@ -49,12 +49,12 @@ var config = {
             id: 'second-identifier',
             alignment: 'right',
             hidden: false,
-            title: 'Second Title',
-            image: './path/to/image/source.png',
-            description: 'Copy these sections to add to your story.',
+            title: 'Springfield',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/0/0d/City_Hall_-_Springfield%2C_MA_-_DSC03296.JPG',
+            description: 'Population: 153,060. This will talk about the high prevalence of both depression and smoking in Western Massachusetts',
             location: {
-                center: [-77.020636, 38.886900],
-                zoom: 8.5,
+                center: [-72.53952002, 42.11550322],
+                zoom: 11,
                 pitch: 60,
                 bearing: -43.2,
                 // flyTo additional controls-
@@ -67,21 +67,44 @@ var config = {
             mapAnimation: 'flyTo',
             rotateAnimation: true,
             callback: '',
-            onChapterEnter: [],
+            onChapterEnter: [                {
+                layer: 'smoking',
+                opacity: 1,
+                duration: 5000
+            }],
             onChapterExit: []
         },
         {
             id: 'third-identifier',
             alignment: 'left',
             hidden: false,
-            title: 'Third Title',
-            image: './path/to/image/source.png',
-            description: 'Copy these sections to add to your story.',
+            title: 'Newton/Boston Suburbs',
+            image: 'https://upload.wikimedia.org/wikipedia/commons/e/e6/Newton_City_Hall%2C_Massachusetts.jpg',
+            description: 'Population:85,146',
             location: {
-                center: [6.15116, 46.20595],
-                zoom: 12.52,
+                center: [-71.20842576, 42.33160123],
+                zoom: 10,
                 pitch: 8.01,
-                bearing: 0.00
+                bearing: 10
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'fourth-identifier',
+            alignment: 'left',
+            hidden: false,
+            title: 'Fall River',
+            image: '',
+            description: '',
+            location: {
+                center: [-71.10257763, 41.72762402],
+                zoom: 9,
+                pitch: 60,
+                bearing: 10
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
@@ -93,14 +116,14 @@ var config = {
             id: 'fourth-chapter',
             alignment: 'fully',
             hidden: false,
-            title: 'Third Title',
+            title: 'Dover',
             image: './path/to/image/source.png',
-            description: 'Copy these sections to add to your story.',
+            description: 'Population: 2,265',
             location: {
-                center: [-58.54195, -34.71600],
-                zoom: 4,
-                pitch: 0,
-                bearing: 0
+                center: [-71.27416772, 42.24744324],
+                zoom: 12,
+                pitch: 40,
+                bearing: 40
             },
             mapAnimation: 'flyTo',
             rotateAnimation: false,
